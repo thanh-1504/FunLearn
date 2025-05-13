19c6729e0c5d6818948afc98625538b9d86233aa
thanhnhan <dthanhnhan21@gmai.com>
2025-05-14 02:11:45 +0700

    tim kiem va goi y

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/courses", courseRouter);
+app.use("/api/v1/?",timkiem_goiyRouter);
 module.exports = app;
