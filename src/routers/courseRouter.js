0994605c3072914da9a4b0ec2a5efbd27ce37626
nhatthanh <duongnhatthanhaz09@gmail.com>
2025-05-16 16:09:04 +0700

    Thêm phân quyền cho user

--- src/routers/courseRouter.js
+++ src/routers/courseRouter.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const courseController = require("../controllers/courseController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
 router
   .route("/")
   .get(courseController.getAllCourses)
-  .post(courseController.createCourse);
+  .post(authController.authorization(["ADMIN"]), courseController.createCourse);
 
 router
   .route("/:idCourse")
   .get(courseController.findCourseById)
-  .patch(courseController.updateCourse)
-  .delete(courseController.deleteCourse);
+  .patch(authController.authorization(["ADMIN"]), courseController.updateCourse)
+  .delete(
+    authController.authorization(["ADMIN"]),
+    courseController.deleteCourse
+  );
 
 module.exports = router;
